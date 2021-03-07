11cf568390ab129620c26a93e2a656f95d364b8d
GiraffeKOOOO <pstepien@hotmail.co.uk>
2021-03-07 23:41:54 +0000

    - removed JSON nuget package - removed previous components - changed screen size to specification - added screenTitle component to startScreen

--- ContactlessSelfCheckout/ContactlessSelfCheckout/IndexForm.Designer.cs
+++ ContactlessSelfCheckout/ContactlessSelfCheckout/IndexForm.Designer.cs
@@ -28,57 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.screenTitle = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // screenTitle
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F);
-            this.label1.Location = new System.Drawing.Point(531, 365);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(353, 157);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(718, 186);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
-            this.panel1.TabIndex = 1;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Location = new System.Drawing.Point(1115, 406);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.screenTitle.AutoSize = true;
+            this.screenTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.screenTitle.Location = new System.Drawing.Point(415, 24);
+            this.screenTitle.Name = "screenTitle";
+            this.screenTitle.Size = new System.Drawing.Size(178, 73);
+            this.screenTitle.TabIndex = 0;
+            this.screenTitle.Text = "Hello";
+            this.screenTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // IndexForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1470, 835);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.screenTitle);
             this.Name = "IndexForm";
             this.Text = "Contactless Self-checkout";
-            this.Click += new System.EventHandler(this.label1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label screenTitle;
     }
 }
 
--- ContactlessSelfCheckout/ContactlessSelfCheckout/IndexForm.cs
+++ ContactlessSelfCheckout/ContactlessSelfCheckout/IndexForm.cs
@@ -13,32 +13,11 @@ namespace ContactlessSelfCheckout
 {
     public partial class IndexForm : Form
     {
-        string path = @"C:\Users\pstep\Desktop\test.txt";
         public IndexForm()
         {
             InitializeComponent();
             Console.WriteLine("hello world");
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-            Console.WriteLine("p");
-            if (!File.Exists(path))
-            {
-                using (StreamWriter sw = File.CreateText(path))
-                {
-                    sw.WriteLine("Hello my name is Jeff");
-                    sw.WriteLine("Homer SImpson");
-                }
-            }
-            else {
-                using (StreamReader sr = File.OpenText(path)) {
-                    string fileInput;
-                    while ( (fileInput = sr.ReadLine()) != null ) {
-                        Console.WriteLine(fileInput);
-                    }
-                }
-            }
-        }
     }
 }
