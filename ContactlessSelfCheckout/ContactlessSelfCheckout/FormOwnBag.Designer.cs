ec509530e7d2f9655ff10b55921e86658a7a44c1
GiraffeKOOOO <pstepien@hotmail.co.uk>
2021-03-23 00:51:47 +0000

    - added FormOwnBag - added bagging item image - added instruction labels - added help button, which takes the user to the help screen - added a start button, which takes the user to the basket list screen - this screen is finished and ready for merge - this completes the start screen

---
+++ ContactlessSelfCheckout/ContactlessSelfCheckout/FormOwnBag.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ContactlessSelfCheckout
+{
+    partial class FormOwnBag
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormOwnBag));
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblInstruction.Location = new System.Drawing.Point(249, 524);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(517, 31);
+            this.lblInstruction.TabIndex = 10;
+            this.lblInstruction.Text = "Please place your bags in the baggin area";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Silver;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnStart.Location = new System.Drawing.Point(429, 619);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(153, 90);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.BackColor = System.Drawing.Color.Silver;
+            this.btnHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnHelp.Location = new System.Drawing.Point(12, 627);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(118, 90);
+            this.btnHelp.TabIndex = 11;
+            this.btnHelp.Text = "Help";
+            this.btnHelp.UseVisualStyleBackColor = false;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(229, 575);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(556, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Then press the Start button once you\'re done";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(187, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(635, 491);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormOwnBag
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.btnStart);
+            this.Name = "FormOwnBag";
+            this.Text = "Contactless Self-Checkout";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
